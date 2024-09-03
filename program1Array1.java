d7f931be26cf612d8c631f7fc4df8670c89be56a
tushar jadhav <132814429+JadhavTushar7@users.noreply.github.com>
2024-09-03 15:08:14 +0530

    Add files via upload

---
+++ program1Array1.java
@@ -0,0 +1,29 @@
+class ArrayDemo {
+
+        public static  void main(String[] args) {
+
+        int arr1[] = new int[10];        // int arr1[] = {0,1,2,3};
+
+                        arr1[0] = 10;
+                        arr1[1] = 20;
+                        arr1[2] = 30;
+                        arr1[3] = 40;
+			arr1[4] = 50;                                                                                                           arr1[5] = 60;
+	         	arr1[6] = 70;
+			arr1[7] = 80;                                                                                                           arr1[8]=  90;
+			arr1[9] = 100; 
+                        
+		      
+			System.out.println(arr1[0]);
+                           System.out.println(arr1[1]);
+                            System.out.println(arr1[2]);
+                             System.out.println(arr1[3]);
+                              System.out.println(arr1[4]);
+                               System.out.println(arr1[5]);
+                                System.out.println(arr1[6]);
+                                 System.out.println(arr1[7]);
+                                  System.out.println(arr1[8]);
+                                    System.out.println(arr1[9]);
+                            
+        }
+}
