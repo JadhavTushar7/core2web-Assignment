1c4352d8d8cf090161fc88999b93f9b6a41f1258
tushar jadhav <132814429+JadhavTushar7@users.noreply.github.com>
2024-09-03 20:23:10 +0530

    Add files via upload

---
+++ program0001.java
@@ -0,0 +1,15 @@
+class ArrayDemo01 {
+
+
+        public static void main(String[]args){
+
+
+
+              int arr1[] = {10,20,30,40,50,60,70,80,90,100}; 
+
+            for (int i = 0; i < arr1.length; i++) {
+    System.out.println(arr1[i]);
+         }
+
+	}
+}
