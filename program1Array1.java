bc0d4ab6d563be9bdc41c8dd17f8d2f997652f3a
tushar jadhav <132814429+JadhavTushar7@users.noreply.github.com>
2024-09-03 15:09:36 +0530

    Update program1Array1.java

--- program1Array1.java
+++ program1Array1.java
@@ -2,7 +2,7 @@ class ArrayDemo {
 
         public static  void main(String[] args) {
 
-        int arr1[] = new int[10];        // int arr1[] = {0,1,2,3};
+        int arr1[] = new int[10];        // int arr1[] = {0,1,2,3,4,5,6,7,8,9};
 
                         arr1[0] = 10;
                         arr1[1] = 20;
@@ -15,15 +15,15 @@ class ArrayDemo {
                         
 		      
 			System.out.println(arr1[0]);
-                           System.out.println(arr1[1]);
-                            System.out.println(arr1[2]);
-                             System.out.println(arr1[3]);
-                              System.out.println(arr1[4]);
-                               System.out.println(arr1[5]);
-                                System.out.println(arr1[6]);
-                                 System.out.println(arr1[7]);
-                                  System.out.println(arr1[8]);
-                                    System.out.println(arr1[9]);
+                        System.out.println(arr1[1]);
+                        System.out.println(arr1[2]);
+                        System.out.println(arr1[3]);
+                        System.out.println(arr1[4]);
+                        System.out.println(arr1[5]);
+                        System.out.println(arr1[6]);
+                        System.out.println(arr1[7]);
+                        System.out.println(arr1[8]);
+                        System.out.println(arr1[9]);
                             
         }
 }
